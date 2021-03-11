73064555d7e3b3009c59b4740f7f8fd79ec54f76
Mr30nama <77214247+Mr30nama@users.noreply.github.com>
2021-03-11 17:12:57 +0000

    Published

---
+++ script.js
@@ -0,0 +1,53 @@
+//vars
+var copyBusy = false;
+
+// functions
+const backBtn = () => {
+	let el = document.getElementsByClassName("article");
+	for (let i = 0; i < el.length; i++) {
+		el[i].classList.remove("show-article");
+	}
+	document.getElementById("main-article").classList.add("show-article");
+};
+const showMacArticle = () => {
+	document.getElementById("main-article").classList.remove("show-article");
+	document.getElementById("mac-article").classList.add("show-article");
+};
+
+function SelectText(element) {}
+
+// $(function () {
+// 	$("p").click(function () {
+// 		SelectText("selectme");
+// 	});
+// });
+
+const copyToClipboard = (element) => {
+	if (copyBusy) return;
+	copyBusy = true;
+	var doc = document,
+		text = doc.getElementById(element),
+		range,
+		selection;
+	if (doc.body.createTextRange) {
+		range = document.body.createTextRange();
+		range.moveToElementText(text);
+		range.select();
+	} else if (window.getSelection) {
+		selection = window.getSelection();
+		range = document.createRange();
+		range.selectNodeContents(text);
+		selection.removeAllRanges();
+		selection.addRange(range);
+	}
+
+	/* Copy the text inside the text field */
+	document.execCommand("copy");
+
+	/* Alert the copied text */
+	document.getElementById("copy-title-termial-1").classList.add("show-title");
+	setTimeout(() => {
+		document.getElementById("copy-title-termial-1").classList.remove("show-title");
+		copyBusy = false;
+	}, 2000);
+};

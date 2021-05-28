4ac6e45d075a674432bc0b55745968d7ad86129a
Neon <jesspinklet@outlook.com>
2021-05-28 16:59:01 +0430

    Added linux section

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //vars
 var copyBusy = false;
+var copyBusy2 = false;
 
 // functions
 const backBtn = () => {
@@ -50,5 +51,34 @@ const copyToClipboard = (element) => {
 	setTimeout(() => {
 		document.getElementById("copy-title-termial-1").classList.remove("show-title");
 		copyBusy = false;
+	}, 2000);
+};
+const copyToClipboard2 = (element) => {
+	if (copyBusy2) return;
+	copyBusy2 = true;
+	var doc = document,
+		text = doc.getElementById(element),
+		range,
+		selection;
+	if (doc.body.createTextRange) {
+		range = document.body.createTextRange();
+		range.moveToElementText(text);
+		range.select();
+	} else if (window.getSelection) {
+		selection = window.getSelection();
+		range = document.createRange();
+		range.selectNodeContents(text);
+		selection.removeAllRanges();
+		selection.addRange(range);
+	}
+
+	/* Copy the text inside the text field */
+	document.execCommand("copy");
+
+	/* Alert the copied text */
+	document.getElementById("copy-title-termial-2").classList.add("show-title");
+	setTimeout(() => {
+		document.getElementById("copy-title-termial-2").classList.remove("show-title");
+		copyBusy2 = false;
 	}, 2000);
 };

ce27913e83d74eebe0719f561e74a75629042453
EternalC0der <s.eternalcoder@gmail.com>
2023-03-23 21:28:20 +0330

    Update

--- _nuxt/config.8e1e7db6.js
+++
@@ -1,1 +0,0 @@
-import{Q as n,i,p as o}from"./entry.c10e539d.js";const f={},t=n(f);function s(){const p=i();return p._appConfig||(p._appConfig=o(t)),p._appConfig}export{s as u};
--- _nuxt/config.e684ec6a.js
+++
@@ -1,1 +0,0 @@
-import{Q as n,i,p as o}from"./entry.1d70b21d.js";const f={},t=n(f);function s(){const p=i();return p._appConfig||(p._appConfig=o(t)),p._appConfig}export{s as u};

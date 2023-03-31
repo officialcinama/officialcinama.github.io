02fa56ad3eeccf33da95ccc524834be76f6edd8a
Mr30nama <77214247+Mr30nama@users.noreply.github.com>
2023-03-31 12:08:48 +0100

    TV

---
+++ _nuxt/BrandLogo.vue.3a8197f3.js
@@ -0,0 +1,1 @@
+import{d as c,M as n,o as s,e as l,g as e,N as r}from"./entry.90890705.js";const m=["src"],_=c({__name:"BrandLogo",props:{compact:{type:Boolean},color:null},setup(o){const a=n("theme","dark"),{logo:t}=r;return(g,p)=>(s(),l("img",{draggable:"false",src:e(t)(o.color||e(a),o.compact),alt:"30nama logo"},null,8,m))}});export{_};
---
+++ script.js
@@ -0,0 +1,80 @@
+//vars
+var copyBusy = false;
+var copyBusy2 = false;
+
+// functions
+const backBtn = () => {
+	window.location = '/'
+};
+const showArticles = (page) => {
+	page = page + "-article";
+	document.getElementById("main-article").classList.remove("show-article");
+	document.getElementById(page).classList.add("show-article");
+};
+
+function SelectText(element) {}
+
+// $(function () {
+// 	$("p").click(function () {
+// 		SelectText("selectme");
+// 	});
+// });
+
+const copyToClipboard = (element) => {
+	if (copyBusy) return;
+	copyBusy = true;
+	var doc = document,
+		text = doc.getElementById(element),
+		range,
+		selection;
+	if (doc.body.createTextRange) {
+		range = document.body.createTextRange();
+		range.moveToElementText(text);
+		range.select();
+	} else if (window.getSelection) {
+		selection = window.getSelection();
+		range = document.createRange();
+		range.selectNodeContents(text);
+		selection.removeAllRanges();
+		selection.addRange(range);
+	}
+
+	/* Copy the text inside the text field */
+	document.execCommand("copy");
+
+	/* Alert the copied text */
+	document.getElementById("copy-title-termial-1").classList.add("show-title");
+	setTimeout(() => {
+		document.getElementById("copy-title-termial-1").classList.remove("show-title");
+		copyBusy = false;
+	}, 2000);
+};
+const copyToClipboard2 = (element) => {
+	if (copyBusy2) return;
+	copyBusy2 = true;
+	var doc = document,
+		text = doc.getElementById(element),
+		range,
+		selection;
+	if (doc.body.createTextRange) {
+		range = document.body.createTextRange();
+		range.moveToElementText(text);
+		range.select();
+	} else if (window.getSelection) {
+		selection = window.getSelection();
+		range = document.createRange();
+		range.selectNodeContents(text);
+		selection.removeAllRanges();
+		selection.addRange(range);
+	}
+
+	/* Copy the text inside the text field */
+	document.execCommand("copy");
+
+	/* Alert the copied text */
+	document.getElementById("copy-title-termial-2").classList.add("show-title");
+	setTimeout(() => {
+		document.getElementById("copy-title-termial-2").classList.remove("show-title");
+		copyBusy2 = false;
+	}, 2000);
+};
